16872f2bbf6e6899daaf5f9a1ce77420ecd38d37
Asekur <lina.rusinovich@mail.ru>
2020-10-05 00:38:13 +0300

    Created file copying service

---
+++ Lab #2/Pool Thread/FileCopy.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+
+namespace Pool_Thread
+{
+    class FileCopy
+    {
+        private static int _amountDirectories = 0;
+        private static int _amountCopiedFiles = 0;
+        public static List<string> pathSourceFiles = new List<string>();
+        public static void StartCopy(string source, string dest, string file)
+        {
+            if (_amountDirectories != 0)
+            {
+                Directory.CreateDirectory(file.Replace(source, dest));
+                _amountDirectories--;
+            }
+            else
+            {
+                string fileName = file.Replace(source, dest);
+                File.Copy(file, fileName, true);
+            }
+            _amountCopiedFiles++;
+        }
+
+        public static List<string> GetAmountInSource(string source)
+        {
+            IEnumerable<string> allfiles = Directory.EnumerateFiles(source, "*.*", SearchOption.AllDirectories);
+            IEnumerable<string> allfolders = Directory.EnumerateDirectories(source, "*.*", SearchOption.AllDirectories);
+            //add all directories to list
+            foreach (string folder in allfolders)
+            {
+                pathSourceFiles.Add(folder);
+                _amountDirectories++;
+            }
+            //add all files to list
+            foreach (string filename in allfiles)
+            {
+                pathSourceFiles.Add(filename);
+            }
+            return pathSourceFiles;
+        }
+
+        public static int GetResultAmount()
+        {
+            return _amountCopiedFiles;
+        }
+    }
+}
---
+++ Lab #2/Pool Thread/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+
+namespace Pool_Thread
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string source, dest;
+
+            Console.Write("Source: ");
+            source = Console.ReadLine();
+            Console.Write("Dest: ");
+            dest = Console.ReadLine();
+            //add task of copying files
+
+            if (!Directory.Exists(source) || !Directory.Exists(dest))
+            {
+                Console.WriteLine("Not existing directory");
+                Console.ReadKey();
+                return;
+            }
+
+            //create pool thread
+            const int amountThreads = 100;
+            TaskQueue newThreads = new TaskQueue(amountThreads);
+            Console.WriteLine();
+
+            //while non-copyied files in source exists
+            foreach (var file in FileCopy.GetAmountInSource(source))
+            {
+                Directory.CreateDirectory(dest);
+                newThreads.EnqueueTask(() => FileCopy.StartCopy(source, dest, file));
+            }
+            newThreads.DisposePool();
+
+            Console.ReadKey();
+            if (newThreads.Count() == 0)
+            {
+                Console.WriteLine("\nAll tasks have been completed. Total: " + FileCopy.GetResultAmount());
+            }
+            Console.ReadKey();
+        }
+    }
+}
