f102aac2e0d82380f5adf130c7c25a19512a9d35
Asekur <lina.rusinovich@mail.ru>
2020-11-02 14:55:30 +0300

    Added small fixes

--- Lab 4/ParallelWait/Program.cs
+++ Lab 4/ParallelWait/Program.cs
@@ -6,17 +6,19 @@ namespace ParallelWait
     {
         const int maxIndex = 20;
         public delegate void TaskDelegate();
+        //private static Logger _log = LogManager.GetCurrentClassLogger();
 
         static void Main(string[] args)
         {
             ParallelWait parallelWait = new ParallelWait();
 
-            int counter = 0;
+            //от -2^31 до 2^31-1
+            int counter = 1;
             ParallelWait.TaskDelegate[] delegates = new ParallelWait.TaskDelegate[maxIndex];
 
             for (int i = 0; i < maxIndex; i++)
             {
-                delegates[i] = (() => counter++);
+                delegates[i] = (() => counter *= 2);
             }
 
             parallelWait.WaitAll(delegates);
